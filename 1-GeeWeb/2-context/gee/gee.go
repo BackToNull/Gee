0fb9ed544e8228e99e9aa21258f6130c6a3a0b4e
zhufengliang <1227579896@qq.com>
2025-03-06 11:50:56 +0800

    2-context

--- 1-GeeWeb/2-context/gee/gee.go
+++ 1-GeeWeb/2-context/gee/gee.go
@@ -1,7 +1,30 @@
 package gee
 
+import "net/http"
+
 type HandlerFunc func(*Context)
 
 type Engine struct {
 	router *router
+}
+
+func New() *Engine {
+	return &Engine{newRouter()}
+}
+
+func (engine *Engine) GET(pattern string, handlerFunc HandlerFunc) {
+	engine.router.addRoute("GET", pattern, handlerFunc)
+}
+
+func (engine *Engine) POST(pattern string, handlerFunc HandlerFunc) {
+	engine.router.addRoute("POST", pattern, handlerFunc)
+}
+
+func (engine *Engine) Run(port string) (err error) {
+	return http.ListenAndServe(port, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handler(c)
 }
